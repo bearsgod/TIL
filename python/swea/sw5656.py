36e1a75b8aadf8271d80563a6348f01f13e07788
ysw <bearsgod@gmail.com>
2019-01-14 17:52:14 +0900

    D09 | 190114 PM | HTML

--- python/swea/sw5656.py
+++ python/swea/sw5656.py
@@ -1,59 +1,30 @@
-import copy,itertools
-def clear(ls,x,y):
-    global w,h
-    num=ls[x][y]
-    if num==1:
-        ls[x][y]=0
-    elif num>1:
-        for left in range(num-1):
-            if x-left<0:
-                break
-            else:
-                clear(ls,x-left,y)
-        for right in range(1,num):
-            if x+right>w-1:
-                break
-            else:
-                clear(ls,x+right,y)
-        for down in range(1,num):
-            if y+down>h-1:
-                break
-            else:
-                clear(ls,x,y+down)
-        ls[x][y]=0
+def clearing(ls,x,y):
+    value=ls[x][y]
+    if value==1:
+        checkboard[x][y]=0
+    elif value>1:
+        if value-2>y:
+            for i in range()
+
+def search(idx,ls):
+    global n,w,h,minnum
+    if idx==n:
+        summary=0
+        for i in range(h):
+            summary+=ls[i].count(0)
+        if minnum>summary:
+            minnum=summary
+    else:
+        if trial[0]>0:
+            v=trial-1
+            trial[0]-=1
+            
+            
 for t in range(int(input())):
     n,w,h=list(map(int,input().split()))
-    gamerow=[]
-    for line in range(h):
-        rows=list(map(int,input().split()))
-        gamerow.append(rows)
-    gamepan=[]
-    for i in range(w):
-        gamecolumn=[]
-        for j in range(h):
-            gamecolumn.append(gamerow[j][i])
-        gamepan.append(gamecolumn)
-    where_to_shot=itertools.product([x for x in range(w)],repeat=n)
-    minnum=w*h
-    for pro in where_to_shot:
-        a=copy.deepcopy(gamepan)
-        counter=0
-        for shot in pro:
-            for high in range(h):
-                if a[shot][high]:
-                    clear(a,shot,high)
-                    break
-            for row2 in range(w):
-                for m in range(h-1,-1,-1):
-                    if not a[row2][m]:
-                        index=m
-                        break
-                for delete in range(h-m-1):
-                    a[row2][delete]=0
-        for weight in range(w):
-            for height in range(h):
-                if a[weight][height]:
-                    counter+=1
-        if minnum>counter:
-            minnum=counter
-    print(f'#{t+1} {minnum}')
+    gameboard=[]
+    for row in range(h):
+        gameboard.append(list(map(int,input().split())))
+    checkboard=[[1 for width in range(w)] for height in range(h)]
+    minnum=180
+    trial=[w for tring in range(n)]
