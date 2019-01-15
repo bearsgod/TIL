03c7729c5e67a745490dc2d143f6e0c47fdb5210
ysw <bearsgod@gmail.com>
2019-01-15 17:46:37 +0900

    D10 | 190115 PM | CSS

--- python/swea/sw2105.py
+++ python/swea/sw2105.py
@@ -2,16 +2,51 @@ for t in range(int(input())):
     lines=[]
     for n in range(int(input())):
         lines.append(list(map(int,input().split())))
-    minnum=0
-    for i in range(n-2):
-        for j in range(1,n-1):
-            leftmax=j
-            rightmax=n-1-j
+    base=n+1
+    maxnum=-1
+    goleft=goright=0
+    for i in range(base-2):
+        for j in range(1,base-1):
+            limit=n-i
+            if j>=limit:
+                leftmax=limit-1
+            else:
+                leftmax=j
             dessertlist=[]
             cursor=lines[i][j]
             dessertlist.append(cursor)
             for ld in range(leftmax,0,-1):
+                dessertlist=dessertlist[:1]
                 for a in range(1,ld+1):
                     cursor=lines[i+a][j-a]
                     dessertlist.append(cursor)
-                for rd in range(rightmax,0,-1):
+                goleft=a
+                for rd in range(limit-ld,0,-1):
+                    try:
+                        for b in range(1,rd+1):
+                            cursor=lines[i+a+b][j-a+b]
+                            dessertlist.append(cursor)
+                        goright=b
+                        for c in range(1,a+1):
+                            cursor=lines[i+a+b-c][j-a+b+c]
+                            dessertlist.append(cursor)
+                        for d in range(1,b+1):
+                            if i+a+b-c-d==i:
+                                break
+                            cursor=lines[i+a+b-c-d][j-a+b+c-d]
+                            dessertlist.append(cursor)
+                    except IndexError:
+                        dessertlist=dessertlist[:goleft+1]
+                        continue
+                    token=1
+                    for e in dessertlist:
+                        if dessertlist.count(e)>1:
+                            token=0
+                            break
+                    if token:
+                        temp=len(dessertlist)
+                        if maxnum<temp:
+                            maxnum=temp
+                    print(dessertlist)
+                    dessertlist=dessertlist[:goleft+1]
+    print(f'#{t+1} {maxnum}')
